9279bc3b5cb5fc88390c785f1e319e782cf46c95
Victor <vitico222@gmail.com>
2026-07-27 19:36:45 -0400

    términos y condiciones espacios

--- syllabus-terms.js
+++ syllabus-terms.js
@@ -1,5 +1,5 @@
 export const termsContent = {
   title: "Terms and Conditions",
   description:
-    "General conditions for sticker allocation: \n\n1. Stickers are awarded based on active participation, completing classroom challenges, and consistent effort. \n2. Each unlocked casilla represents a milestone achieved in your learning path. \n3. Keep your profile secure with your PIN and enjoy your progress on the Orange Energy Board!",
+    "General conditions for sticker allocation: \n\n1. Stickers are awarded based on active participation, completing classroom challenges, and consistent effort. \n\n2. Each unlocked casilla represents a milestone achieved in your learning path. \n\n3. Keep your profile secure with your PIN and enjoy your progress on the Orange Energy Board!",
 };
