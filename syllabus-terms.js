4d36da74a3eea5f8da52153359755420eeb56a02
Victor <vitico222@gmail.com>
2026-08-01 16:37:58 -0400

    terms and conditions 1

--- syllabus-terms.js
+++ syllabus-terms.js
@@ -1,5 +1,5 @@
 export const termsContent = {
   title: "Terms and Conditions",
   description:
-    "General conditions for sticker allocation: \n\n1. Stickers are awarded based on active participation, completing classroom challenges, and consistent effort. \n\n2. Each unlocked casilla represents a milestone achieved in your learning path. \n\n3. Keep your profile secure with your PIN and enjoy your progress on the Orange Energy Board!",
+    "\n1. Stickers are awarded by the teachers based on active participation, completing challenges, and consistent effort. \n\n2. Prize winners are officially accredited by the teachers and coordinators, based on the student's overall performance. \n\n3. To be awarded a prize, all previous challenges must be successfully completed. \n\n4. Prizes are subject to availability and will be awarded based on current stock. \n\n5. The academy reserves the right to organize, modify, or cancel the challenges and prizes at any time, and is under no obligation to award stickers or prizes. \n\n6. © 2026 IFL. All rights reserved.",
 };
