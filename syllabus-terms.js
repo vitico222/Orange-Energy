f56915c41ff25cba7c1a83015ef9be64750bd8c8
Victor <vitico222@gmail.com>
2026-08-22 06:57:56 -0400

    scroll en terminos y condiciones

--- syllabus-terms.js
+++ syllabus-terms.js
@@ -1,5 +1,5 @@
 export const termsContent = {
-  title: "Terms and Conditions",
+  title: "Terms & Conditions",
   description:
     "\n1. Stickers are awarded by the teachers based on active participation, completing challenges, and consistent effort. \n\n2. Prize winners are officially accredited by the teachers and coordinators, based on the student's overall performance. \n\n3. To be awarded a prize, all previous challenges must be successfully completed. \n\n4. Prizes are subject to availability and will be awarded based on current stock. \n\n5. The academy reserves the right to organize, modify, or cancel the challenges and prizes at any time, and is under no obligation to award stickers or prizes. \n\n6. © 2026 IFL. All rights reserved.",
 };
